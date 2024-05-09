46b30c4a342a888887dc2ea1c90634f84f6c65a1
Mario Cares <mario.cares.c@outlook.com>
2024-05-09 17:57:09 -0400

    Change port

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 import posts from "../routes/posts";
 
-const app = new Hono()
+const app = new Hono();
 
-app.route('/publicacion', posts);
+app.route("/publicacion", posts);
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 4000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
